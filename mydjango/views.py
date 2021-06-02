d5ce3575e8e0ea5e2bd70809ed0f29fd3d51e5c3
Nikitha <nikithagaddam1109@gmail.com>
2021-06-02 21:38:31 +0530

    fits commit

---
+++ mydjango/views.py
@@ -0,0 +1,18 @@
+#importing
+from django.shortcuts import render
+from .forms import RegistrationForm
+from django.contrib import messages
+
+# Create your views here
+
+def registration(request):
+    if request.method == 'POST':
+        fm = RegistrationForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(request,
+                             'Registration Created Successfully')
+    else:
+        fm = RegistrationForm()
+    return render(request,
+                  'registration.html',{'fm':fm})
